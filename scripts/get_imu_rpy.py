5590c9046e15747588232a746ba276b60a600977
zzy33 <2235354854@qq.com>
2021-04-08 20:52:42 +0800

    update scripts/get_imu_rpy.py.

--- scripts/get_imu_rpy.py
+++ scripts/get_imu_rpy.py
@@ -10,7 +10,7 @@ def callback(data):
     #这个函数是tf中的,可以将四元数转成欧拉角
     (r,p,y) = tf.transformations.euler_from_quaternion((data.orientation.x,data.orientation.y,data.orientation.z,data.orientation.w))
     #由于是弧度制，下面将其改成角度制看起来更方便
-    rospy.loginfo("Pitch = %f,Roll = %f, Yaw = %f",r*180/3.1415926,p*180/3.1415926,y*180/3.1415926)
+    rospy.loginfo("Roll = %f, Pitch = %f, Yaw = %f",r*180/3.1415926,p*180/3.1415926,y*180/3.1415926)
 
 def get_imu():
     rospy.init_node('get_imu', anonymous=True)
