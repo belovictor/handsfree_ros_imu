eb22ca02dccfc112cd48ac1c3b511f902a818658
zzy33 <2235354854@qq.com>
2021-09-08 16:54:22 +0800

    Modify some unit errors

--- demo/linux/hfi_b9.py
+++ demo/linux/hfi_b9.py
@@ -49,7 +49,7 @@ def handleSerialData(raw_data):
 
         elif buff[1] == 0x52 and pub_flag[1]:
             if checkSum(data_buff[0:10], data_buff[10]):
-                angularVelocity = [hex_to_short(data_buff[2:10])[i] / 32768.0 * 2000 for i in range(0, 3)]
+                angularVelocity = [hex_to_short(data_buff[2:10])[i] / 32768.0 * 2000 * math.pi / 180 for i in range(0, 3)]
 
             else:
                 print('0x52 校验失败')
